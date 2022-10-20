048a13c4ac19796c1b33d00367a1863e69156866
sneha.shaji <sneha.shaji@qburst.com>
2022-10-20 18:04:39 +0530

    implemented onclick mouseover on office addresses

--- olyxio.js
+++ olyxio.js
@@ -20,7 +20,7 @@ const testimonialsData = async () => {
             </div>
         </div>
         <div class="saying-content">
-            <p>
+            <p>console.log(data);
             ${element.description}
             </p>
         </div>
@@ -46,7 +46,6 @@ const navbarItems = async () => {
       const menuItem = data["sidenav"];
       menuItem.forEach((element) => {
         const icon = document.createElement("i");
-        // icon.setAttribute("id", "icon");
         icon.setAttribute("class", "fa fa-angle-down");
         const parent = document.getElementById("dropdown");
         if (element.subMenu) {
@@ -96,50 +95,68 @@ const regions = async () => {
     .then((response) => response.json())
     .then((data) => {
       const locations = data["location"];
+      localStorage.setItem("locationData", JSON.stringify(locations));
       generateSubContinents(locations);
-      continentAddress(data)
+      continentAddress(data);
     });
 };
 regions();
 
 function generateSubContinents(locations) {
-  // const locations = data["location"];
   locations.forEach((element) => {
     const parent = document.querySelector(".region");
     const subContinent = document.createElement("a");
     subContinent.setAttribute("class", "region-heading");
     subContinent.innerHTML = `${element.region}`;
-    subContinent.setAttribute("onclick","continentAddress(this)");
+    subContinent.setAttribute("onclick", "officeAddress(this.innerHTML)");
     subContinent.href = "#";
     parent.appendChild(subContinent);
   });
 }
 
-function continentAddress(data){
-  console.log(data);
-  const locations = data["location"];
-  locations.forEach((element) => {
-    const parent = document.querySelector(".contact-page-content");
-    const subContinent = document.createElement("div");
-    subContinent.setAttribute("class", `${element.region}`.toLowerCase());
-    parent.appendChild(subContinent);
-    const address = document.createElement("div");
-    address.setAttribute("class", "address");
-    subContinent.appendChild(address);
-    element.place.forEach((item) => {
-      const regionBox = document.createElement("div");
-      regionBox.setAttribute("class", "region-box");
-      address.appendChild(regionBox);
-      const regionBoxName = document.createElement("h4");
-      regionBoxName.innerHTML = `${item.name}`;
-      regionBox.appendChild(regionBoxName);
-      Object.keys(item.address).forEach((lines) => {
-        const regionBoxLines = document.createElement("p");
-        regionBoxLines.innerHTML = item.address[lines];
-        regionBox.appendChild(regionBoxLines);
-      });
-      address.appendChild(regionBox);
+function continentAddress(data) {
+  const parent = document.querySelector(".contact-page-content");
+  const subContinent = document.createElement("div");
+  subContinent.setAttribute("class", `${data.region}`.toLowerCase());
+  parent.replaceChildren(subContinent);
+  const address = document.createElement("div");
+  address.setAttribute("class", "address");
+  subContinent.appendChild(address);
+  data.place.forEach((item) => {
+    const regionBox = document.createElement("div");
+    regionBox.setAttribute("class", "region-box");
+    regionBox.setAttribute("onmouseover", "locationImageChange(this)");
+    regionBox.setAttribute("data-officeimg", `${item.locationImage}`);
+    address.appendChild(regionBox);
+    const regionBoxName = document.createElement("h4");
+    regionBoxName.innerHTML = `${item.name}`;
+    regionBox.appendChild(regionBoxName);
+    Object.keys(item.address).forEach((lines) => {
+      const regionBoxLines = document.createElement("p");
+      regionBoxLines.innerHTML = item.address[lines];
+      regionBox.appendChild(regionBoxLines);
     });
-    // address.appendChild(regionBox);
+    if (item.phoneNumber) {
+      const phone = document.createElement("p");
+      phone.innerHTML = `<i class="fa fa-phone"></i> ${item.phoneNumber}`;
+      regionBox.appendChild(phone);
+    }
+    address.appendChild(regionBox);
   });
+  const locationImage = document.createElement("div");
+  locationImage.setAttribute("class", "location-img");
+  subContinent.appendChild(locationImage);
+}
+
+function officeAddress(data) {
+  const location = JSON.parse(localStorage.getItem("locationData"));
+  const continentData = location.find((loc) => loc.region === data);
+  continentAddress(continentData);
+}
+
+function locationImageChange(regionTag) {
+  const imgBox = document.querySelector(".location-img");
+  const img = document.createElement("img");
+  img.src = regionTag.dataset.officeimg;
+  imgBox.replaceChildren(img);
 }
